24f89a95b57b183ca31c7c97f008bc3d9d0b3f3b
Jocsan <jocsanmejia00@gmail.com>
2024-09-28 10:31:35 -0600

    ci: adding image to the ship (character)

--- Game/personaje.py
+++ Game/personaje.py
@@ -10,6 +10,8 @@ class Cubo:
         self.alto = 50  # Alto del cubo.
         self.velocidad = 10  # Velocidad del cubo.
         self.color = "red"  # Color del cubo.
+        self.imagen = pygame.image.load("Game/img/navePygame.png")
+        self.imagen = pygame.transform.scale(self.imagen, (self.ancho, self.alto))
         
         # Crea un rectángulo para representar el cubo en pantalla.
         self.rect = pygame.Rect(self.x, self.y, self.ancho, self.alto)
@@ -20,4 +22,6 @@ class Cubo:
         self.rect = pygame.Rect(self.x, self.y, self.ancho, self.alto)
         
         # Dibuja un rectángulo en la ventana con las propiedades del cubo.
-        pygame.draw.rect(ventana, self.color, self.rect)
+        #pygame.draw.rect(ventana, self.color, self.rect)
+
+        ventana.blit(self.imagen, (self.x, self.y))
